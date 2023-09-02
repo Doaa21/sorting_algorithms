885e821e9fc16c76268fb2d3b8785e7970a881af
Doaa21 <doaaelhaweet2121@gmail.com>
2023-09-02 01:17:54 -0700

    0x1B. C - Sorting algorithms & Big O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,39 @@
 #include "sort.h"
 /**
- * bubble_sort - sort array lements from min to max value
- * @array: array
- * @size: array size
+ * swap - swaps two integer numbers
+ * @a: integer a
+ * @b: integer b
+ * Return: void
+ */
+void swap(int *a, int *b)
+{
+int temp;
+temp = *a;
+*a = *b;
+*b = temp;
+}
+/**
+ * bubble_sort - sorts a integer of array using bubble sort algorithm
+ * @array: a integer of array
+ * @size: size of array
+ * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
-
-	size_t i, index, tmp = 0;
-
-	if (size < 2)
-		return;
-	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
-		{
-			if (array[index] > array[index + 1] && array[index + 1])
-			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
-			}
-		}
+size_t i, j, flag = 0;
+if (size < 2)
+return;
+for (i = 0; i < size - 1 && flag == 0; i++)
+{
+flag = 1;
+for (j = 0; j < size - 1 - i; j++)
+{
+if (array[j] > array[j + 1])
+{
+swap(&array[j], &array[j + 1]);
+print_array(array, size);
+flag = 0;
+}
+}
+}
 }
